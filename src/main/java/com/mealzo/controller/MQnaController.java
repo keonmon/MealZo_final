27f67779c8b0028f5ca155fc4496a2c5b7d967a0
WIN\gmral <gmralwnr@hanmail.net>
2022-03-29 11:11:06 +0900

    MQna  마이페이지 이동

--- src/main/java/com/mealzo/controller/MQnaController.java
+++ src/main/java/com/mealzo/controller/MQnaController.java
@@ -1,7 +1,13 @@
 package com.mealzo.controller;
 
+import java.util.HashMap;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.mealzo.service.MQnaService;
 
@@ -10,6 +16,20 @@ public class MQnaController {
 
 	@Autowired
 	MQnaService qs;
+	
+	@RequestMapping("/mypageForm")
+	public String mypageForm(HttpServletRequest request  ) {
+		HttpSession session = request.getSession();
+		HashMap<String, Object> loginUser =
+				(HashMap<String, Object>)session.getAttribute("loginUser");
+
+		if(loginUser==null) {
+			return "member/login";
+		}else
+			
+		
+		return "mypage/mypage";
+	}
 	
 	
 }
