fab9162cd04605c17ff17f9d4703f1b327dcffea
han sunghee <tjdgml0319@nate.com>
2022-03-31 10:40:53 +0900

    [한성희] 새로운시작

---
+++ src/main/java/com/mealzo/dto/addressVO.java
@@ -0,0 +1,15 @@
+package com.mealzo.dto;
+
+import lombok.Data;
+
+@Data
+public class addressVO {
+
+	private String zip_num;
+	private String sido;
+	private String gugun;
+	private String dong;
+	private String zip_code;
+	private String bunji;
+
+}
